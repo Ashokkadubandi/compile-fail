578c4728a0fac0c5778a310064f710b2769e20a2
Ashok <velmaashok765@gmail.com>
2023-03-06 10:17:26 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+// Write your code here
+import {Component} from 'react'
+import Message from './Message/index'
+
+console.log(message)
+
+class Home extends Component {
+  render() {
+    return <div>HEllo</div>
+  }
+}
+export default Home
